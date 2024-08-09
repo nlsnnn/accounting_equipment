4c7163f06060e97d030157f4ca5d23ef48f01201
nlsnnn <lagged.06@mail.ru>
2024-08-09 15:20:52 +0300

    feat: added permissions

---
+++ api/main/permissions.py
@@ -0,0 +1,9 @@
+from rest_framework import permissions #BasePermission, SAFE_METHODS
+from rest_framework.request import Request
+
+
+class IsAdminOrRead(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and request.user.is_admin
--- api/main/views.py
+++ api/main/views.py
@@ -2,18 +2,22 @@ from rest_framework.viewsets import ModelViewSet
 
 from .models import Stock, Category, Equipment
 from .serializers import StockSerializer, CategorySerializer, EquipmentSerializer
+from .permissions import IsAdminOrRead
 
 
 class StockView(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
+    permission_classes = [IsAdminOrRead]
 
 
 class CategoryView(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = [IsAdminOrRead]
 
 
 class EquipmentView(ModelViewSet):
     queryset = Equipment.objects.all()
     serializer_class = EquipmentSerializer
+    permission_classes = [IsAdminOrRead]
