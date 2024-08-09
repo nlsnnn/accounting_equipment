657d70f70727d080eff66e794394f6d9aa106bd3
nlsnnn <lagged.06@mail.ru>
2024-08-09 15:07:15 +0300

    feat: added viewsets

--- api/main/urls.py
+++ api/main/urls.py
@@ -1,5 +1,14 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
+from .views import StockView, CategoryView, EquipmentView
+
+
+router = DefaultRouter()
+router.register('stock', StockView)
+router.register('cat', CategoryView)
+router.register('equipment', EquipmentView)
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
--- api/main/views.py
+++ api/main/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from .models import Stock, Category, Equipment
+from .serializers import StockSerializer, CategorySerializer, EquipmentSerializer
+
+
+class StockView(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+
+
+class CategoryView(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class EquipmentView(ModelViewSet):
+    queryset = Equipment.objects.all()
+    serializer_class = EquipmentSerializer
