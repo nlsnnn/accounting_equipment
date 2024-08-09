384fb75ead37bcdb11f16578885fda93489dba44
nlsnnn <lagged.06@mail.ru>
2024-08-09 14:59:18 +0300

    feat: added models

--- api/main/models.py
+++ api/main/models.py
@@ -2,12 +2,23 @@ from django.db import models
 
 
 class Stock(models.Model):
-    pass
+    name = models.CharField(verbose_name='Название', max_length=250)
+    address = models.CharField(verbose_name='Адресс', max_length=250)
+    capacity = models.PositiveIntegerField(verbose_name='Вместимость')
+    time_created = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    time_updated = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
 
 
 class Category(models.Model):
-    pass
+    name = models.CharField(verbose_name='Название', max_length=250)
+    description = models.TextField(verbose_name='Описание', blank=True, null=True)
+    time_created = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    time_updated = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
 
 
 class Equipment(models.Model):
-    pass
+    name = models.CharField(verbose_name='Навзание', max_length=250)
+    cat = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Категория')
+    stock = models.ForeignKey(Stock, on_delete=models.PROTECT, verbose_name='Склад')
+    time_created = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    time_updated = models.DateTimeField(verbose_name='Дата изменения', auto_now=True)
