38098d2893719beea350de55269c1cffa00d7780
nlsnnn <lagged.06@mail.ru>
2024-08-09 15:46:44 +0300

    feat: added JWT authentication

--- api/main/permissions.py
+++ api/main/permissions.py
@@ -6,4 +6,9 @@ class IsAdminOrRead(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        return request.user.is_authenticated and request.user.is_admin
+        return request.user.is_authenticated and request.user.is_superuser
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated and request.user.is_superuser
--- api/main/urls.py
+++ api/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from .views import StockView, CategoryView, EquipmentView
 
@@ -10,5 +11,8 @@ router.register('cat', CategoryView)
 router.register('equipment', EquipmentView)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/', TokenVerifyView.as_view(), name='token_verify')
 ]
